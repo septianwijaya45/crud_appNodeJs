e403c3a60321186de9e50db108ee7c5a4fa90a92
Septian Wijaya Aminulloh <septianwijaya170845@gmail.com>
2021-08-15 04:27:50 +0700

    configuration project structure (mcv), http server & modules

---
+++ server.js
@@ -0,0 +1,37 @@
+const express       = require('express');
+const dotenv        = require('dotenv');
+const morgan        = require('morgan');
+const bodyParser    = require('body-parser');
+const path          = require('path');
+
+const app = express();
+
+// ***** DOTENV setting ***** //
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080;
+
+// ***** log request status ***** //
+app.use(morgan('tiny'));
+
+// ***** Body Parser Configuration ***** //
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ***** View Configuration ***** //
+app.set('view engine', 'ejs');
+// app.set('views', path.resolve(__dirname, 'views/ejs'));
+
+// **** Load Asset ***** //
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
+
+
+// ***** Routes ***** //
+app.get('/', (req, res) => {
+    res.send('CRUD application');
+});
+
+// ***** LISTEN APP ***** //
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
