abd786ffeaa8227b0078cb9e2997cb1c19b1d0ad
Septian Wijaya Aminulloh <septianwijaya170845@gmail.com>
2021-08-18 05:17:42 +0700

    CRUD User Browser

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,13 @@
+const axios = require('axios');
+
 exports.homeRoutes = (req, res) => {
-    res.render('index');
+    axios.get('http://localhost:3000/api/users')
+        .then(function(response){
+            res.render('index', {data: response.data});
+        })
+        .catch(err => {
+            res.send(err);
+        })
 };
 
 exports.addUserRoutes = (req, res) => {
@@ -7,5 +15,13 @@ exports.addUserRoutes = (req, res) => {
 };
 
 exports.updateUserRoutes = (req, res) => {
-    res.render('update_user');
+    axios.get('http://localhost:3000/api/users', {params: {id: req.query.id}})
+        .then(function(userdata){
+            res.render('update_user', {
+                user: userdata.data
+            })
+        })
+        .catch(err => {
+            res.send(err);
+        })
 };
